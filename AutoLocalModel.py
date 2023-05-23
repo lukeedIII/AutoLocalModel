4074e02c3e4f8f8c0a7ec506ea12b23aedbf7863
LukeedIII <67829747+lukeedIII@users.noreply.github.com>
2023-05-23 22:46:08 +0200

    Update AutoLocalModel.py

--- AutoLocalModel.py
+++ AutoLocalModel.py
@@ -10,6 +10,28 @@ from langchain.agents import AgentType
 from langchain.llms import LlamaCpp
 from langchain.memory import ConversationBufferMemory, ConversationSummaryMemory, ConversationTokenBufferMemory, ConversationSummaryBufferMemory
 
+# Custom Memory Class
+class CustomMemory:
+    def __init__(self, llm, max_token_limit):
+        self.llm = llm
+        self.max_token_limit = max_token_limit
+        self.memory = []
+
+    def add(self, item):
+        self.memory.append(item)
+        if len(self.memory) > self.max_token_limit:
+            self.memory.pop(0)
+
+    def get(self):
+        return self.memory
+
+# Custom Tool Class
+class CustomTool(Tool):
+    def run(self, input):
+        # Your custom tool code here
+        output = "Custom tool output: " + input
+        return output
+
 # Load environment variables
 MODEL_PATH = os.getenv("LLAMACPP_MODEL", "models/default/model.bin")
 TEMP = float(os.getenv("TEMP", 1.0))
@@ -19,11 +41,17 @@ AI_GOAL = os.getenv("AI_GOAL", "default_goal")
 # Initialize LlamaCpp model
 llm = LlamaCpp(model_path=MODEL_PATH, temperature=TEMP, n_ctx=NCTX)
 
-# Initialize memory
-memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=4098)  # Summary and Token Limit Memory
+# Initialize custom memory
+memory = CustomMemory(llm=llm, max_token_limit=4098)
 
 # Load tools
 tools = load_tools(["google-search", "requests_all", "wikipedia", "human"], llm=llm)
+
+# Initialize custom tool
+custom_tool = CustomTool(llm=llm)
+
+# Add custom tool to tools list
+tools.append(custom_tool)
 
 print("Initializing agent...")
 react = initialize_agent(tools, llm, memory=memory, n_batch=8, agent="zero-shot-react-description", verbose=True, return_intermediate_steps=True)
